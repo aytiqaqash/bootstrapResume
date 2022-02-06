bbc1f49469498e0c8e24c7ab3cf5f3f653b0bd24
aytiqaqash <aytiqaqash@gmail.com>
2022-02-05 20:56:30 -0500

    FRONT. Static olaraq bütün səhifələri əlavə olundu və qismən routelar düzəldi.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+Route::get('/blog', function () {
+    return view('pages.blog');
+});
+Route::get('/resume', function () {
+    return view('pages.resume');
+});
+Route::get('/portfolio', function () {
+    return view('pages.portfolio');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
