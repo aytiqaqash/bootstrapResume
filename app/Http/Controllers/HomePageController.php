ff995a5a2a3862507a9301711144929551b74a34
aytiqaqash <aytiqaqash@gmail.com>
2022-04-24 23:41:47 -0400

    2.12 Services Model, Migration and Controller

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Education;
 use App\Models\Experience;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
@@ -12,6 +13,7 @@ class HomePageController extends Controller
         $summary = \App\Models\Summary::first();
         $educations = Education::get();
         $experiences = Experience::get();
-        return view('pages.home',compact('summary','educations', 'experiences'));
+        $services = Service::get();
+        return view('pages.home',compact('summary','educations', 'experiences','services'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -43,49 +43,17 @@
         <h6 class="section-subtitle">WHAT I DO</h6>
         <h2 class="section-title">SERVICES</h2>
         <div class="row">
-            <div class="media service-card col-lg-6">
+            @foreach($services as $service)
+                <div class="media service-card col-lg-6">
                 <div class="service-icon">
-                    <img src="assets/images/001-target.svg" alt="target">
+                    <img src="{{Storage::url($service->image)}}" alt="target">
                 </div>
                 <div class="media-body">
-                    <h5 class="service-title">web designing</h5>
-                    <p class="service-description">Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed
-                        tortor.</p>
+                    <h5 class="service-title">{{$service->title}}</h5>
+                    <p class="service-description">{{$service->description}}</p>
                 </div>
             </div>
-            <div class="media service-card col-lg-6">
-                <div class="service-icon">
-                    <img src="assets/images/003-idea.svg" alt="bulb">
-                </div>
-                <div class="media-body">
-                    <h5 class="service-title">Graphic design</h5>
-                    <p class="service-description">Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed
-                        tortor.
-                    </p>
-                </div>
-            </div>
-            <div class="media service-card col-lg-6">
-                <div class="service-icon">
-                    <img src="assets/images/002-development.svg" alt="development">
-                </div>
-                <div class="media-body">
-                    <h5 class="service-title">Development</h5>
-                    <p class="service-description">Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed
-                        tortor.
-                    </p>
-                </div>
-            </div>
-            <div class="media service-card col-lg-6">
-                <div class="service-icon">
-                    <img src="assets/images/004-smartphone.svg" alt="smartphone">
-                </div>
-                <div class="media-body">
-                    <h5 class="service-title">Mobile design</h5>
-                    <p class="service-description">Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed
-                        tortor.
-                    </p>
-                </div>
-            </div>
+            @endforeach
         </div>
     </section>
     <section class="testimonial-section">
